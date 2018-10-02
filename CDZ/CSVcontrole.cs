4fd56155d180a7115d54d404b95c493c62bd9d99
BCoaracy <bruno.coaracy@gmail.com>
2018-10-02 09:38:01 -0300

    Primeira alteracao

---
+++ CDZ/CSVcontrole.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CDZ
+{
+    class CSVcontrole
+    {
+        public List<Star> DezMagnitudes()
+        {//10 maiores magnitudes
+            StreamReader stream = new StreamReader(@"D:\\Facul\\Persistencia\\hypparcos\\hygdata\\hygdata_v3.csv");
+            List<Star> listaEstrelas = new List<Star>();
+
+            try
+            {
+                string linha = null;
+                string[] coluna = null;
+                stream.ReadLine();
+
+                for (int i = 0; i <= 10; i++)
+                {
+                    linha = stream.ReadLine();
+                    coluna = linha.Split(',');
+                    Star star = new Star(coluna[0], coluna[6], Convert.ToDouble(coluna[13], CultureInfo.InvariantCulture));
+                    listaEstrelas.Add(star);
+
+                }
+
+                listaEstrelas.RemoveAt(0);
+
+                var listaOrdenada = listaEstrelas.OrderByDescending(m => m.getMag());
+
+                for (int i = 0; i < listaOrdenada.Count(); i++)
+                {
+                    Star estrela = listaOrdenada.ElementAt(i);
+
+                    Console.WriteLine("id: " + estrela.getId() + " Magnitude: " + estrela.getMag() + " Nome: " + estrela.getNome());
+                    
+                }
+
+                return listaOrdenada.ToList();
+
+            }
+            catch (IOException Ioe)
+            {
+                return null;
+                MessageBox.Show("Erro!!" + Ioe);
+            }
+            finally
+            {
+                stream.Close();
+            }
+
+        }
+
+        public void estrelasVisiveis()
+        {
+            StreamReader reader = new StreamReader(@"D:\\Facul\\Persistencia\\hypparcos\\hygdata\\hygdata_v3.csv");
+            StreamWriter writer = new StreamWriter(@"C:\\Users\\Bruno\\Desktop\\EstralasVisiveis.csv");
+
+            try
+            {
+                string linha = null;
+                string[] coluna = null;
+                reader.ReadLine();
+
+                while (reader.ReadLine() != null)
+                {
+                    linha = reader.ReadLine();
+                    coluna = linha.Split(',');
+                    //var x = Convert.ToDouble(coluna[13], CultureInfo.InvariantCulture);
+
+
+                    if (Convert.ToDouble(coluna[13], CultureInfo.InvariantCulture) < 6.5)
+                    {
+                        writer.WriteLine(coluna[0] + ", " + coluna[6] + ", " + coluna[13]);
+                    }
+                }
+
+            }
+            catch (Exception Ioe)
+            {
+                MessageBox.Show("Erro: " + Ioe);
+            }
+            finally
+            {
+                reader.Close();
+                writer.Close();
+            }
+
+        }
+
+        public List<Star> maisProximas()
+        {
+            StreamReader reader = new StreamReader(@"D:\\Facul\\Persistencia\\hypparcos\\hygdata\\hygdata_v3.csv");
+
+            List<Star> listaEstrelas = new List<Star>();
+
+            try
+            {
+                string linha = null;
+                string[] coluna = null;
+                reader.ReadLine();
+
+                while (reader.ReadLine() != null)
+                {
+                    linha = reader.ReadLine();
+                    coluna = linha.Split(',');
+                    Star star = new Star(coluna[0], coluna[6], Convert.ToDouble(coluna[13], CultureInfo.InvariantCulture),
+                        Convert.ToDouble(coluna[9], CultureInfo.InvariantCulture));
+                    listaEstrelas.Add(star);
+
+                }
+
+                var listaDistantes = listaEstrelas.OrderByDescending(s => s.getDist());
+
+                for (int i = 0; i <= 10; i++)
+                {
+                    var estrela = listaDistantes.ElementAt(i);
+                    Console.WriteLine("Id: " + estrela.getId() + " Nome: " + estrela.getNome() + " Distância: " + estrela.getDist());
+                }
+
+                return listaDistantes.ToList();
+
+            }
+
+            catch (Exception Ioe)
+            {
+                return null;
+                MessageBox.Show("Erro: " + Ioe);
+            }
+            finally
+            {
+                reader.Close();
+            }
+
+        }
+
+        public List<string[]> pesquisaConstelacao(string constelacao)
+        {
+            StreamReader reader = new StreamReader(@"D:\\Facul\\Persistencia\\hypparcos\\hygdata\\hygdata_v3.csv");
+            List<string[]> listaConstelacao = new List<string[]>();
+            try
+            {
+                string linha = null;
+                string[] coluna = null;
+
+                reader.ReadLine();
+
+                while (reader.ReadLine() != null)
+                {
+                    linha = reader.ReadLine();
+                    coluna = linha.Split(',');
+                    
+                    if (coluna[29].Equals(constelacao))
+                    {
+                        listaConstelacao.Add(coluna);
+                    }
+                }
+
+                return listaConstelacao;
+            }
+            catch (Exception Ioe)
+            {
+                MessageBox.Show("Erro: " + Ioe);
+                return null;
+            }
+            finally
+            {
+                reader.Close();
+                
+            }
+        }
+        public string[] agulhaEscarlate()
+        {
+            List<string[]> listaSco = new List<string[]>();
+            listaSco = pesquisaConstelacao("Sco");
+
+           
+
+            List<Star> starScoList = new List<Star>();
+            foreach(var estrela in listaSco)
+            {
+                Star star = new Star(estrela[0], estrela[6], Convert.ToDouble(estrela[13], CultureInfo.InvariantCulture));
+                starScoList.Add(star);
+            }
+            var listaScoOrdenada = starScoList.OrderBy(x => x.getMag());
+            var id  = listaScoOrdenada.First().getId();
+            foreach(var estrela in listaSco)
+            {
+                if (id.Equals(estrela[0]))
+                {
+                    return estrela;
+                }
+            }
+            return null;
+        }
+
+    }
+
+    class Star {
+
+        string id { get; set; }
+        string proper { get; set; }
+        double mag { get; set; }
+        double dist { get; set; }
+
+        public Star(string i, string nome, double magnitude)
+        {
+            this.id = i;
+            this.proper = nome;
+            this.mag = magnitude;
+        }
+
+        public Star(string i, string nome, double magnitude, double distancia)
+        {
+            this.id = i;
+            this.proper = nome;
+            this.mag = magnitude;
+            this.dist = distancia;
+        }
+
+        public double getMag()
+        {
+            return this.mag;
+        }
+
+        public string getNome()
+        {
+            return this.proper;
+        }
+
+        public string getId()
+        {
+            return this.id;
+        }
+
+        public double getDist()
+            {
+                return this.dist;
+            }
+    }
+
+}
--- CDZ/Form1.cs
+++ CDZ/Form1.cs
@@ -16,5 +16,64 @@ namespace CDZ
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtSaida.Text = null;
+            var csv = new CSVcontrole();
+            List<Star> starsList = csv.DezMagnitudes();
+            foreach(var s in starsList)
+            {
+                txtSaida.AppendText("id: " + s.getId() + " Magnitude: " + s.getMag() + " Nome: " + s.getNome() + "\n" );
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtSaida.Text = null;
+            var csv = new CSVcontrole();
+            csv.estrelasVisiveis();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtSaida.Text = null;
+            var csv = new CSVcontrole();
+            List<Star> starsList = csv.maisProximas();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                var estrela = starsList.ElementAt(i);
+                txtSaida.AppendText("Id: " + estrela.getId() + " Nome: " + estrela.getNome() + " Distância: " + estrela.getDist() + "\n");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            txtSaida.Text = null;
+            var csv = new CSVcontrole();
+            List<string[]> lista = new List<string[]>();
+            lista = csv.pesquisaConstelacao(txtConst.Text);
+            foreach (string[] s in lista)
+            {
+                i++;
+                txtSaida.AppendText("id da estrela: " + s[0] + " | Nome : " + s[6] + " | Constelação: " + s[29] + "\n");
+            }
+            txtSaida.AppendText(Convert.ToString(i));
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var csv = new CSVcontrole();
+            var yoga = csv.agulhaEscarlate();
+            txtSaida.Text="Estrela mais brilhante de escorpião: " + Convert.ToString(yoga[6]);
+            Console.WriteLine(yoga[6]);
+        }
+
+        private void txtSaida_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
